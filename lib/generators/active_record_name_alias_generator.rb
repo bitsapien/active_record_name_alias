1c312ecc38ccc4ec37bc0831032b39beb448040b
C Rahul <crahul@elitmus.com>
2016-12-26 20:30:21 +0530

    added migration

--- lib/generators/active_record_name_alias_generator.rb
+++ lib/generators/active_record_name_alias_generator.rb
@@ -1,5 +1,14 @@
 require 'rails/generators/base'
 
 class ActiveRecordNameAliasGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+	source_root File.expand_path('../templates', __FILE__)
+	def copy_migration
+		migration_template "migration.rb", "db/migrate/name_alias_create_#{table_name}.rb", migration_version: migration_version
+	end
+
+	def migration_version
+		if rails5?
+			"[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+		end
+	end
 end
