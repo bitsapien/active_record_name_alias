e0086057f3db563b2c0251d9c6187a7f5540c289
C Rahul <crahul@elitmus.com>
2016-12-26 19:42:06 +0530

    added code in main file

---
+++ lib/active_record_name_alias.rb
@@ -0,0 +1,10 @@
+require 'rails'
+require 'active_support/core_ext/numeric/time'
+require 'active_support/dependencies'
+require 'orm_adapter'
+
+module ActiveRecordNameAlias
+	def self.setup
+    yield self
+  end
+end
